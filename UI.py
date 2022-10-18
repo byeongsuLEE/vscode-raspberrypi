27216c4319f1872235b5f92944eb99192548b38c
Byeongsu Lee <evil55@naver.com>
2022-10-18 20:55:28 +0900

    button click test

--- UI.py
+++ UI.py
@@ -1,3 +1,5 @@
+# pyqt 설치 및 ui 사용 
+##https://okjh.tistory.com/75
 import sys
 from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtWidgets import QApplication, QDialog
@@ -9,7 +11,17 @@ class MyQtProgramming(QDialog):
         loadUi('practiceui.ui',self)
         self.setWindowTitle("Button Show")
         
+    # 버튼 클릭시 실행되는 코드 
+    @pyqtSlot()
+    def on_pushButton_released(self):
+        self.setWindowTitle("사용자 등록창")
         
+    def on_pushButton_2_released(self):
+        self.setWindowTitle("문 열기창")
+        
+    def on_pushButton_3_released(self):
+        self.setWindowTitle("침입기록")  
+                      
 app=QApplication(sys.argv)
 widget=MyQtProgramming()
 widget.show()
